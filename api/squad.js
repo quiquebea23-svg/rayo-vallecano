7c766c0dbd029212e1f7531cd1c97c7cae874086
quiquebea23-svg <quiquebea23@gmail.com>
2026-03-20 00:53:12 +0100

    Update squad.js

--- api/squad.js
+++ api/squad.js
@@ -1,12 +1,17 @@
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET');
+  res.setHeader('Cache-Control', 's-maxage=86400'); // 24h cache
 
   const url = 'https://v3.football.api-sports.io/players/squads?team=728';
-  const response = await fetch(url, {
-    headers: { 'x-apisports-key': process.env.APIFOOTBALL_KEY }
-  });
-
-  const data = await response.json();
-  res.status(response.status).json(data);
+  try {
+    const response = await fetch(url, {
+      headers: { 'x-apisports-key': process.env.APIFOOTBALL_KEY }
+    });
+    const data = await response.json();
+    // Return players array with id, name, number, photo
+    const players = data.response?.[0]?.players || [];
+    res.json({ players });
+  } catch(e) {
+    res.json({ players: [], error: e.message });
+  }
 }
